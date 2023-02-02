1e4311043c4fd2ce0025f681ad99ad8063748f6a
PokoenkoS <pokoenkosveta@gmail.com>
2023-02-02 18:16:20 +0200

    add

--- src/index.js
+++ src/index.js
@@ -3,52 +3,66 @@ import Notiflix from 'notiflix';
 
 const API_KEY = `33261865-905999929b5f445e8a29b592f`;
 const form = document.querySelector(`.search-form`);
-const btn = document.querySelector(`button`);
-const input = document.querySelector('[name="searchQuery"]');
 const container = document.querySelector(`.gallery`);
 const loadBtn = document.querySelector(`.load-more`);
-let page = 1;
+let queryPage = 1;
+let inputValue = "";
 
 loadBtn.hidden = true;
 
 form.addEventListener(`submit`, onSubmit);
 loadBtn.addEventListener(`click`, onLoadMore);
-input.addEventListener(`input`, onInput);
+
 
 function fetchPhoto (name) {
-    const url = axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${name}&orientation=horizontal&safesearch=true&image_type=photo&page=1&per_page=40`);
+    const url = axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${name}&orientation=horizontal&safesearch=true&image_type=photo&page=${queryPage}&per_page=40`);
     
     return url;
    }
-
+  
  function onSubmit(e) {
     e.preventDefault();
-    const form = e.currentTarget;
-    const inputValue = form.elements.searchQuery.value.trim();
     loadBtn.hidden = true;
+    clearForm();
    
-    
+    const form = e.currentTarget;
+    inputValue = form.elements.searchQuery.value.trim();
+   
+    queryPage = 1;      
     fetchPhoto(inputValue)
-    .then(res => createMarkup(res.data.hits))
     .then((res) => {
-        if (res = []){
-            return Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.')
-        } if (res.totalHits === 0) {
-            return Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`) 
+        console.log(res);
+        if (res.data.hits.length ===0){
+            Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.')
         }
-    })
+        createMarkup(res.data.hits)
+        loadBtn.hidden = false;
+     })
     .catch(error => {
-        clearForm();
-      
+   
     })
-    loadBtn.hidden = false;
- };
+   };
 
- function createMarkup(picture) {
+function onLoadMore() {
+    queryPage += 1;
+    inputValue = form.elements.searchQuery.value.trim();
+    fetchPhoto(inputValue)
+    .then((res) => createMarkup(res.data.hits))
+    .catch(error => {
+   
+    })
+}
+
+function clearForm() {
+    container.innerHTML = '';
+   
+}
+
+function createMarkup(picture) {
     
     const cardList = picture.reduce((acc, {webformatURL, tags, likes, views, comments, downloads}) => {
     return acc + `<div class="photo-card">
-      <img src=${webformatURL} alt=${tags} width = 300 loading="lazy" />
+      <img src=${webformatURL} alt=${tags} height = 250 loading="lazy" />
       <div class="info">
         <p class="info-item">
           <b>Likes: ${likes}</b>
@@ -65,18 +79,6 @@ function fetchPhoto (name) {
       </div>
     </div> `
  },"" )
- console.log(cardList);
- return container.insertAdjacentHTML(`afterbegin`, cardList);
- }
-function onLoadMore() {
-    page += 1;
-    fetchPhoto().then((res) =>{
-        createMarkup(res.data.hits)
-        page += 1;
-    }) 
-}
-function clearForm() {
-    container.innerHTML = '';
-   
-}
 
+ return container.insertAdjacentHTML(`beforeend`, cardList);
+ }
