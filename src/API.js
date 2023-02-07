356632bf4d4eb90852968cabcb524df8d8db9488
PokoenkoS <pokoenkosveta@gmail.com>
2023-02-07 22:23:12 +0200

    fixed

--- src/API.js
+++ src/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const BASE_URL = `https://pixabay.com/api/`;
 const API_KEY = `33261865-905999929b5f445e8a29b592f`;
 
-export default async function fetchPhoto (name, queryPage = 1, per_page=180) {
+export default async function fetchPhoto (name, queryPage = 1, per_page=40) {
     const url = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${name}&orientation=horizontal&safesearch=true&image_type=photo&page=${queryPage}&per_page=${per_page}`);
     
     return url;
